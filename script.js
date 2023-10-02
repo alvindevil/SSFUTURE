11cc15ae6652551e9ba014bf06a948f4398e6466
alvin devils <alvindevil568@gmail.com>
2023-10-02 16:26:01 +0530

    3rd commit - adding responsive hamberger menu

---
+++ script.js
@@ -0,0 +1,14 @@
+const menu_toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+
+menu_toggle.addEventListener('click', ()=>
+{
+    if(nav.classList.contains('active')){
+        nav.classList.remove('active');
+        menu_toggle.classList.remove('menu-cross');
+    }
+    else{
+        nav.classList.add('active');
+        menu_toggle.classList.add('menu-cross');
+    }
+})
