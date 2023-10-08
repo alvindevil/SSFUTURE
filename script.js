7ac94147e38b7f9c23bb952d563bc93bc762940e
alvin devils <alvindevil568@gmail.com>
2023-10-08 14:46:45 +0530

    adding android dev services section -again

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ menu_toggle.addEventListener('click', ()=>
         menu_toggle.classList.add('menu-cross');
     }
 })
+
+function showAppCategory(category) {
+    var slides = document.querySelectorAll('.app-slide');
+    slides.forEach(function(slide) {
+        slide.style.display = 'none';
+    });
+
+    var activeSlides = document.querySelectorAll('.' + category);
+    activeSlides.forEach(function(slide) {
+        slide.style.display = 'block';
+    });
+
+    var buttons = document.querySelectorAll('.tab-button');
+    buttons.forEach(function(button) {
+        button.classList.remove('active');
+    });
+
+    var activeButton = document.querySelector('.' + category + '-button');
+    activeButton.classList.add('active');
+}
+
